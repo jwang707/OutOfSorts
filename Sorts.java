3f0424a00f61fd4d6da9bdfe10da952c992d4ba8
Jasmine <jwang2367@gmail.com>
2020-12-16 22:23:40 -0500

    made a final check for sorted perfection, still have some de of the bugging left potetially

--- Sorts.java
+++ Sorts.java
@@ -1,6 +1,5 @@
 import java.util.Arrays;
 
-
 public class Sorts{
   /**Bubble sort of an int array.
   *@postcondition The array will be modified such that the elements will be in increasing order.
@@ -13,17 +12,20 @@ public class Sorts{
     while (!sorted) {
 
       for (int i = 0; i < unsorted; i ++) { // runs through the code a pair at a time
+      //  System.out.println("a full pass");
+      //  System.out.println();
         if (data[i] > data[i+1]) { // if it's out of order
           sorted = false;
           int yeet = data[i];
           data[i] = data[i+1];
           data[i+1] = yeet;
-          System.out.println(Arrays.toString(data));
+        //  System.out.println(Arrays.toString(data));
+      //    System.out.println(unsorted + "Something was sorted");
           }
         else {
           i++;
-
-          System.out.println(Arrays.toString(data));
+    //      System.out.println(Arrays.toString(data));
+      //    System.out.println(unsorted + "progress as normal");
         }
       }
 
@@ -34,8 +36,17 @@ public class Sorts{
           }
         }
 
-      if (unsorted <= 0) {
-        sorted = true;
+      if (unsorted <= 0) { // go through one more loop before making sorted true
+        boolean allGood = true;
+        for (int i = 0; i < data.length-1; i++) {
+    //      System.out.println("scanning for completion");
+          if (data[i] > data[i+1]){
+            allGood = false;
+
+      //      System.out.println("not done yet");
+          }
+        }
+        sorted = allGood;
       }
     }
   }
--- bubbleSortTester.java
+++ bubbleSortTester.java
@@ -3,16 +3,35 @@ import java.util.Arrays;
 public class bubbleSortTester{
   public static void main(String[] args){
     int[] casper = new int[]{5, 4, 3, 1, 2};
-/*
-    System.out.println(casper.length);
+
+    System.out.println("testing casper");
     System.out.println(Arrays.toString(casper));
     Sorts.bubbleSort(casper);
-    System.out.println(Arrays.toString(casper)); // basic checks */
+    System.out.println(Arrays.toString(casper)); // basic checks
 
     int[] efficiency = new int[]{2, 1, 3, 4, 5};
+    System.out.println();
+
+    System.out.println("testing efficiency");
     System.out.println(Arrays.toString(efficiency));
     Sorts.bubbleSort(efficiency);
     System.out.println(Arrays.toString(efficiency));
 
+    int[] perf = new int[]{1, 2, 3, 4, 5};
+    System.out.println();
+
+    System.out.println("testing pre-sorted");
+    System.out.println(Arrays.toString(perf));
+    Sorts.bubbleSort(perf);
+    System.out.println(Arrays.toString(perf));
+
+/*
+        int[] empty = new int[]{};
+        System.out.println();
+
+        System.out.println("testing empty");
+        System.out.println(Arrays.toString(empty));
+        Sorts.bubbleSort(empty);
+        System.out.println(Arrays.toString(empty)); */ // empty
   }
 }
