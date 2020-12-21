0b02b752c5b23c1d7f876d2e69f16a2bfe41b75d
Jasmine <jwang2367@gmail.com>
2020-12-21 18:18:15 -0500

    insertionSort works!

--- Sorts.java
+++ Sorts.java
@@ -19,10 +19,43 @@ public class Sorts{
       //  System.out.println(unsorted + "Something was sorted");
           }
       }
-
       if (countSorts == 0) {
         j = data.length;
       }
       }
     }
-  }
+
+    /**selection sort of an int array.
+*@postcondition The array will be modified such that the elements will be in increasing order.
+*@param data - the elements to be sorted.
+*/
+ public static void selectionSort(int[] data){
+   for (int j = 0; j < data.length-1; j++) {
+     int min = data[j];
+     for (int i = j; i < data.length; i++) {
+       if (min > data[i]) {
+         int yeet = min;
+         min = data[i];
+         data[i] = yeet;
+       }
+     }
+     data[j] = min;
+   }
+ }
+
+ /**insertion sort of an int array.
+*@postcondition The array will be modified such that the elements will be in increasing order.
+*@param data - the elements to be sorted.
+*/
+ public static void insertionSort(int[] data){
+   for (int i = 1; i < data.length; i++) {
+     int grab = data[i];
+     for (int j = i-1; j >= 0; j--) {
+       if (grab < data[j]) {
+         data[j+1] = data[j];
+         data[j] = grab;
+       }
+     }
+   }
+ }
+}
